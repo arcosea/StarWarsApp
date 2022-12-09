95c923bdeb7a2b9ba09fcab2f206ca8095eedfe4
StuartGavidia <stuarta0403@hotmail.com>
2022-12-09 13:09:49 -0800

    Add base styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import * as THREE from 'three'
 
 function App() {
 
-  const [flaskData, setFlaskData] = useState("");
-
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/").then((response) => {
-        return response.json()
-    }).then((data) => {
-      setFlaskData(data.value)
-    })
+
+    let scene:THREE.Scene;
+    let camera:THREE.PerspectiveCamera;
+    let renderer:THREE.WebGLRenderer;
+    let starGeo:THREE.BufferGeometry;
+    let stars;
+
+    function init() {
+      //create scene object
+      scene = new THREE.Scene();
+      
+      //setup camera with facing upward
+      camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
+      camera.position.z = 1;
+      camera.rotation.x = Math.PI/2;
+      
+      //setup renderer
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(renderer.domElement);
+
+      starGeo = new THREE.BufferGeometry();
+
+      let verticesArray = []
+
+      for(let i=0;i<18000;i++) {
+
+        let star = Math.random() * 600 - 300
+        verticesArray.push(star)
+        
+      }
+
+      let vertices = Float32Array.from(verticesArray);
+
+      starGeo.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3));
+
+      let sprite = new THREE.TextureLoader().load( 'star.png' );
+      let starMaterial = new THREE.PointsMaterial({
+        color: 0xaaaaaa,
+        size: 0.7,
+        map: sprite
+      });
+
+      stars = new THREE.Points(starGeo,starMaterial);
+      scene.add(stars);
+
+      animate(); 
+    }
+    //rendering loop
+    function animate() {
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    }
+    
+    init();
+
   }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{flaskData}</p>
-      </header>
+      <h1 style={{backgroundColor: "white"}}>Hello</h1>
     </div>
   );
 }
