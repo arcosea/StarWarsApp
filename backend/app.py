8e74e50009e70b1c25dd9ce5e119efc71f5a9b0c
StuartGavidia <stuarta0403@hotmail.com>
2022-12-14 16:04:55 -0800

    Make queries to postgreSQL Handle User Input

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import psycopg2
-from flask import Flask
+from flask import Flask, request
+from psycopg2.extensions import AsIs
 from psycopg2.extras import RealDictCursor
 from flask_cors import CORS
 
@@ -16,6 +17,23 @@ def hello_world():
 
 @app.route('/image-generator')
 def request_from_react():
+
+    with conn.cursor() as cursor:
+
+        star_wars_type = request.args.get("type", "character")
+        star_wars_name = request.args.get("name", "Anakin")
+
+        print(star_wars_type)
+        
+        cursor.execute("""
+        select *
+        from %s
+        where lower(name) like lower(%s)
+        """, [AsIs(star_wars_type), f"%{star_wars_name}%"])
+
+        return list(cursor)
+
+
     #take the user information to then make another request
     #first we get data from db
     #then we request dalle image through helpere dalle_request function
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,21 @@ import './App.css';
 
 function App() {
 
+  //types that are possible
   const typeList:string[] = ["Character", "Planet", "Species", "Starship", "Vehicle"]
 
-  const [type, setType] = useState("")
-
+  //state to track type and name
+  const [type, setType] = useState("character")
   const [name, setName] = useState("")
 
-  const onSubmit = (e:any) => {
+  //handleSubmit
+  const handleSubmit = (e:any) => {
     e.preventDefault()
     
-    fetch("http://127.0.0.1:5000/image-generator", {body: JSON.stringify(
-      {type: type, name: name})}).then((response) => {
+    fetch(`http://127.0.0.1:5000/image-generator?type=${type}&name=${name}`).then((response) => {
         return response.json()
     }).then((data) => {
-        
+        console.log(data)
     })
     
   }
@@ -28,6 +29,7 @@ function App() {
 
   const handleTypeChange = (e:any) => {
     const { value } = e.target
+    console.log(value)
     setType(value)
   }
 
@@ -49,7 +51,7 @@ function App() {
           <select id="type" onChange={handleTypeChange}>
             {
               typeList.map((type, i) => {
-                return <option key={i} value={type}>{type}</option>
+                return <option key={i} value={type.toLowerCase()}>{type}</option>
               })
             }
           </select> 
@@ -58,7 +60,7 @@ function App() {
           Name:
           <input id="name" onChange={handleNameChange} value={name}></input>
         </label>
-        <button onClick={onSubmit}>Search</button>
+        <button onClick={handleSubmit}>Search</button>
       </form>
       <img alt="sunset" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sunset-quotes-21-1586531574.jpg"/>
     </div>
